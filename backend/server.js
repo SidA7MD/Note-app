871987971721e2efcb3c149b063aae186d7870bd
Sidi110 <24110@supnum.mr>
2025-09-28 20:16:34 +0000

    creating some routes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,11 @@
 import express from "express";
+import Noteroute from './Routes/Noteroute.route.js';
 
 const app = express();
 
+app.use('/api/notes' , Noteroute);
 
-app.get('/api/notes', (req,res) => {
-    res.send("you got 5 minutes");
-});
 
-app.post('/api/notes', (req,res) => {
-    res.status(201).json({message : "Note created"});
-});
-
-app.put('/api/notes/:id' , (req,res) => {
-    res.status(200).json({message : "Note Updated"});
-});
-
-app.delete('/api/notes/:id' , (req,res) => {
-    res.status(200).json({message : "Note deleted"});
-});
 
 app.listen(5001, () => {
     console.log('Server Running on Port 5001');
