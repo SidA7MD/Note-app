433bf2076f4b08dc473848521a30720576932494
Sidi110 <24110@supnum.mr>
2025-09-29 15:38:25 +0000

    finished setting up react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router';
+import HomePage from './Pages/HomePage';
+import CreatePage from './Pages/CreatePage';
+import NoteDetailPage from './Pages/NoteDetailPage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div data-theme = 'coffee'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
+    </div>
   )
 }
 
